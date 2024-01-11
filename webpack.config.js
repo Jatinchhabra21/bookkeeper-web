674a283323a3dcdf8156c9132ef1fbea47bda3a9
Jatin Chhabra <57009264+Jatinchhabra21@users.noreply.github.com>
2024-01-11 23:13:17 +0530

    setup eslint and prettier (#8)
    * added tailwind
    
    * added eslint and prettoer
    
    * fixed tailwind issue
    
    * removed package lock file from git ignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,53 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const stylesHandler = 'style-loader';
 
 module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'build'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.(ts|tsx)$/,
-        loader: 'ts-loader',
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'public', 'index.html'),
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'build'),
-    },
-    port: 3000,
-  },
+	entry: './src/index.tsx',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'build'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: ['babel-loader'],
+			},
+			{
+				test: /\.(ts|tsx)$/,
+				loader: 'ts-loader',
+			},
+			{
+				test: /\.css$/i,
+				include: path.resolve(__dirname, 'src'),
+				exclude: /node_modules/,
+				use: [
+					stylesHandler,
+					{
+						loader: 'css-loader',
+						options: {
+							importLoaders: 1,
+						},
+					},
+					'postcss-loader',
+				],
+			},
+		],
+	},
+	resolve: {
+		extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, 'public', 'index.html'),
+		}),
+	],
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'build'),
+		},
+		port: 3000,
+	},
 };
