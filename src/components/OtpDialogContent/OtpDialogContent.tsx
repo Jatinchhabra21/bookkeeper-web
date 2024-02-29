56370ed0347906ff209a1a3cd95cbbf004e50bd6
Jatin Chhabra <jatinc@maqsoftware.com>
2024-02-29 16:46:04 +0530

    updated otp dialog stage content

--- src/components/OtpDialogContent/OtpDialogContent.tsx
+++ src/components/OtpDialogContent/OtpDialogContent.tsx
@@ -1,15 +1,20 @@
 import React, { ChangeEvent, useEffect, useState } from 'react';
 import { Button } from '../../../components/ui/button';
 import { Input } from '../../../components/ui/input';
-import { DialogHeader, DialogTitle } from '../../../components/ui/dialog';
+import {
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+} from '../../../components/ui/dialog';
 import { useAppSelector, useAppDispatch } from '../../store/hooks';
 import constants from '../../constants/OtpDialogContent.constants';
 import { toggleIsSignUpDialogVisible } from '../../store/slices/globalSlice';
-import { DialogStage } from 'src/constants/SignUpDialog.constants';
+import { DialogStage } from '../../constants/SignUpDialog.constants';
+import { OTPInput, SlotProps } from 'input-otp';
+import { cn } from '../../../lib/utils';
 
 type OtpFieldStateType = {
-	value: number | '';
-	error: string | null;
+	value: string;
 };
 
 export type OtpDialogContentProps = {
@@ -23,23 +28,15 @@ export default function OtpDialogContent({
 }: OtpDialogContentProps) {
 	const dispatch = useAppDispatch();
 
-	const [otpField, setOtpField] = useState<OtpFieldStateType>({
-		value: '',
-		error: null,
-	} as OtpFieldStateType);
+	const [otpField, setOtpField] = useState<OtpFieldStateType>(
+		{} as OtpFieldStateType
+	);
+
+	useEffect(() => {
+		console.log(otpField.value);
+	}, [otpField.value]);
 
 	const userEmail = useAppSelector((state) => state.auth.email);
-
-	const validateOtp = () => {
-		if (otpField.value?.toString().length !== constants.OTP_LENGTH)
-			setOtpField((prev) => {
-				return { ...prev, error: constants.OTP_INVALID };
-			});
-		else
-			setOtpField((prev) => {
-				return { ...prev, error: null };
-			});
-	};
 
 	const handleOtpStageChange = () => {
 		if (!otpField.value)
@@ -49,6 +46,33 @@ export default function OtpDialogContent({
 		dispatch(toggleIsSignUpDialogVisible());
 	};
 
+	function Slot(props: SlotProps) {
+		return (
+			<div
+				className={cn(
+					'relative h-14 w-10 text-[2rem]',
+					'flex items-center justify-center',
+					'transition-all duration-300',
+					'border-y border-r border-border first:rounded-l-md first:border-l last:rounded-r-md',
+					'group-focus-within:border-accent-foreground/20 group-hover:border-accent-foreground/20',
+					'outline outline-0 outline-accent-foreground/20',
+					{ 'outline-4 outline-accent-foreground': props.isActive }
+				)}
+			>
+				{props.char !== null && <div>{props.char}</div>}
+				{props.hasFakeCaret && <FakeCaret />}
+			</div>
+		);
+	}
+
+	function FakeCaret() {
+		return (
+			<div className="animate-caret-blink pointer-events-none absolute inset-0 flex items-center justify-center">
+				<div className="h-8 w-px bg-white" />
+			</div>
+		);
+	}
+
 	return (
 		<>
 			<DialogHeader>
@@ -56,29 +80,31 @@ export default function OtpDialogContent({
 					{constants.DIALOG_TITLE} {userEmail}
 				</DialogTitle>
 			</DialogHeader>
-			<div className="flex flex-col gap-1">
-				<div className="flex w-full items-center space-x-2">
-					<Input
-						type="number"
-						placeholder={constants.OTP_INPUT_PLACEHOLDER}
-						value={otpField.value}
-						onChange={(event: ChangeEvent<HTMLInputElement>) =>
-							setOtpField((prev) => {
-								return { ...prev, value: parseInt(event.target.value) };
-							})
-						}
-						onBlur={validateOtp}
-						className={`${otpField.error ? 'border-[1] border-red-600' : ''} flex-1`}
-						autoFocus
+			<div className="my-2 flex flex-col gap-1">
+				<div className="flex w-full items-center justify-center space-x-2">
+					<OTPInput
+						maxLength={6}
+						value={otpField.value?.toString()}
+						onChange={(newValue) => setOtpField({ value: newValue })}
+						render={({ slots }) => {
+							return (
+								<>
+									<div className="flex w-full items-center justify-center">
+										{slots.map((slot, idx) => {
+											return <Slot key={idx} {...slot} />;
+										})}
+									</div>
+								</>
+							);
+						}}
 					/>
-					<Button disabled={!!otpField.error} onClick={handleOtpStageChange}>
-						{constants.CTA_TEXT}
-					</Button>
 				</div>
-				{otpField.error && (
-					<p className="text-xs font-semibold text-red-600">{otpField.error}</p>
-				)}
 			</div>
+			<DialogFooter>
+				<Button disabled={!otpField.value} onClick={handleOtpStageChange}>
+					{constants.CTA_TEXT}
+				</Button>
+			</DialogFooter>
 		</>
 	);
 }
